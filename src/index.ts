78ddfff745e26c5b90f1abec60b7d8fafa61661e
starknt <1431880400@qq.com>
2022-07-18 16:10:09 +0800

    chore: intalize project

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export const one = 1
+
+// eslint-disable-next-line no-console
+console.log('\x1B[93m Hello Typescript!!!\x1B[0m')
---
+++ test/index.spec.ts
@@ -0,0 +1,8 @@
+import { one } from 'pkg-name'
+import { describe, expect, it } from 'vitest'
+
+describe('test one', () => {
+  it('1', () => {
+    expect(one).eq(1)
+  })
+})
